0e68a47e4785fb47a679087ba68d6206ca4312f8
rakeshkumar0016 <webexpert.pixxel@gmail.com>
2024-10-01 13:15:55 +0530

    new

---
+++ src/components/homepage/BannerSection.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+const HomeBanner = () => {
+    
+    return (
+        <div className={'homepage-banner'}>
+           <div className='container'>
+            <div className="connect_banner">
+                <h2>Banner</h2>
+            </div>
+           </div>
+        </div>
+        
+    )
+}
+
+export default HomeBanner; 
+
+
+
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+import BannerSection from "../components/homepage/BannerSection";
+
+function Home() {
+    
+    return (
+
+        <div className={'homepage'}>
+            <BannerSection />       
+        </div>
+    )
+}
+
+
+export default Home
